5cec6c8e04a753af880a5a9c509c388052e1db86
Aditya Surana <aditya.surana@proshore.eu>
2024-10-23 12:28:55 +0545

    content create, delete

--- educa/courses/urls.py
+++ educa/courses/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('create/', views.CourseCreateView.as_view(), name='course_create'),
     path('<int:pk>/edit', views.CourseUpdateView.as_view(), name='course_update'),
     path('<int:pk>/delete', views.CourseDeleteMixin.as_view(), name='course_delete'),
-    path('<int:pk>/module/', views.CourseModuleUpdateView.as_view(), name='course_module_update')
+    path('<int:pk>/module/', views.CourseModuleUpdateView.as_view(), name='course_module_update'),
+    path('module/<int:module_id>/content/<model_name>/create/', views.ContentCreateUpdateView.as_view(), name='module_content_create'),
+    path('module/<int:module_id>/content/<model_name>/<id>/', views.ContentCreateUpdateView.as_view(), name='module_content_update'),
+    path('content/<int:id>/delete/', views.ContentDeleteView.as_view(), name='module_content_delete'),
 ]
